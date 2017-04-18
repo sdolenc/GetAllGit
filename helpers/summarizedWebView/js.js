aa12dfe84c446a99f2c20beab7a3c9eee474e49d
localstepdo <stepdo@microsoft.com>
2017-04-18 09:38:12 -0700

    borrowing part of github.com/sdolenc/Backbone-tut orial

---
+++ helpers/summarizedWebView/js.js
@@ -0,0 +1,84 @@
+'use strict';
+
+//--------------
+// Models
+//--------------
+var Appointment = Backbone.Model.extend();
+
+//--------------
+// Collections
+//--------------
+var AppointmentList = Backbone.Collection.extend({
+    url: 'dyGek.js', // https://codepen.io/cfleschhut/pen/dyGek.js
+    model: Appointment
+});
+
+//--------------
+// Views
+//--------------
+var AppointmentView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'todo',
+
+    template: _.template($('#todo-tmpl').html()),
+
+    events: {
+        'click [type=checkbox]': 'toggleComplete',
+        'click .cancel': 'cancel'
+    },
+
+    initialize: function() {
+        this.model.on('change', this.render, this);
+        this.model.on('remove', this.remove, this);
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    },
+
+    toggleComplete: function() {
+        this.model.set({ complete: !this.model.get("complete") });
+    },
+
+    cancel: function() {
+        this.model.set({ canceled: true });
+        this.collection.remove(this.model);
+    }
+});
+
+var AppointmentListView = Backbone.View.extend({
+    tagName: 'ul',
+    className: 'todos',
+
+    initialize: function() {
+        this.collection.on('sync', this.render, this);
+    },
+
+    render: function() {
+        this.collection.forEach(this.addOne, this);
+        return this;
+    },
+
+    addOne: function(model) {
+        var appointmentView = new AppointmentView({
+            model: model,
+            collection: this.collection
+        });
+        this.$el.append(appointmentView.render().el);
+    },
+
+    remove: function() {
+        this.$el.remove();
+    }
+});
+
+
+var appointments = new AppointmentList();
+appointments.fetch();
+
+var appointmentListView = new AppointmentListView({
+    collection: appointments
+});
+
+$('#app').html(appointmentListView.el);
